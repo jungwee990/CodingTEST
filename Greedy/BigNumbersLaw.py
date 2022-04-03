e0890b07053d35bf80fbd81e30d9d01e0c1dcc61
wjddnl100 <>
2022-04-03 01:40:40 +0000

    큰수의법칙 1번

--- Greedy/BigNumbersLaw.py
+++ Greedy/BigNumbersLaw.py
@@ -8,4 +8,18 @@ second = data[n-2]
 
 result = 0
 
-print("result")
+while True:
+  for i in range(k):
+    if m == 0:
+      break
+
+    result += first
+    m -= 1
+
+  if m == 0:
+    break
+  result += second
+  m -= 1
+  
+
+print(result)
