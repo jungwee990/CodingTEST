1878ddad46bac35dfc8ecbf44812e58165d769a7
ChoJungWee <76547296+jungwee990@users.noreply.github.com>
2022-06-07 22:14:01 +0000

    떡자르기 문제

--- Search/FindParticles.py
+++ Search/FindParticles.py
@@ -44,3 +44,14 @@
 #     print('no', end = ' ')
 
 # 집합자료형 이용
+n = int(input())
+array = set(map(int,input().split()))
+
+m = int(input())
+x = list(map(int,input().split()))
+
+for i in x:
+  if i in array:
+    print("yes", end = ' ')
+  else:
+    print("no", end = ' ')
